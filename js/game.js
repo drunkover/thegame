7294e5ca340d48ccdeafda123324db8f61168012
drunkover <84350817+drunkover@users.noreply.github.com>
2025-03-02 01:25:10 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -114,16 +114,7 @@ function getNextQuestion(allQuestions, chosenLanguages){
             questionBody = document.importNode(pictionaryTemplate.content, true);
             questionBody.querySelector('.question-title').innerText = `${currentPlayer}, Make Them Guess This Image`;
             questionBody.querySelector('div[data-hidden=hidden]').style.display = 'none';
-        //    questionBody.querySelector('img').src = `pictionary/${currentQuestion.image}`;
-// Fetch a random image from the web
- fetch('https://picsum.photos/400/300')
-  .then(response => {
-    questionBody.querySelector('img').src = response.url;
-  })
-  .catch(error => console.error('Error loading the image:', error));
-
-            currentPlayerTurnIndex = (currentPlayerTurnIndex+1) % players.length;
-            break;
+           questionBody.querySelector('img').src = `pictionary/${currentQuestion.image}`;
 
         case DARE:
             if(currentQuestion.num_players > players.length-1){
